1246bca3e3598b6fe08e702adad6120b37c7c2c2
Eric Kimsey <eric.kimsey@icloud.com>
2018-06-04 15:31:59 -0400

    Add LICENSE and license statement to all source files. Change interpreter in source file headings.

--- menu_manager.py
+++ menu_manager.py
@@ -1,7 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
+"""
+Menu System is a shell interface menu manager.
+Copyright (C) 2018  Eric Kimsey
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 # title           :menu_manager.py
-# description     :Manage menu system for Biblion Admin.
+# description     :Manager methods for menu system.
 # author          :Eric Kimsey
 # usage           :
 # notes           :
--- menu_types/__init__.py
+++ menu_types/__init__.py
@@ -1,3 +1,21 @@
+"""
+Menu System is a shell interface menu manager.
+Copyright (C) 2018  Eric Kimsey
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from menu_system.menu_types.base_menu import BaseMenu
 from menu_system.menu_types.option_select import OptionSelect
 from menu_system.menu_types.status_message import StatusMessage
